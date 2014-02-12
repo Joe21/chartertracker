ae695170e4cd546ea535d59ecf3dd14e3e60a60c
Joe21 <joej21@gmail.com>
2014-02-11 19:41:21 -0500

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chartertracker::Application.config.secret_token = '43134af6e4e988f72c9f3505895aa8c49f4e3004abdd13646a90284c4a2f74c6d5f8bc5239c6aa2ab5a5677cdc4fe69db73b3a791fafb82a6fb6ac3e577ff5ff'
