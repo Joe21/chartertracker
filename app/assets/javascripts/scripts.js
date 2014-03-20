7e5f25a6be835cfe43d670963027510ecae49eaa
Joe21 <joej21@gmail.com>
2014-03-20 18:18:28 -0400

    chart reads js object data structure in horizontal chart

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,33 +1,36 @@
-var data = [{"name": "Freeport", "rating": 4}, {"name": "Captree", "rating": 3}, {"name": "World's Fair Marina", "rating": 3}];
+var data = [
+  {"name": "Freeport", "rating": 4},
+  {"name": "Captree", "rating": 3},
+  {"name": "World's Fair Marina", "rating": 3}
+];
 
-var width = 960,
-    height = 500;
+var width = 420,
+  barHeight = 20;
 
 var x = d3.scale.linear()
-		.domain([0,1])
-		.range([0, width]);
-
-var y = d3.scale.linear()
-		.domain([0, d3.max("y", function(d) {return data.rating; })]);
+  .domain([0, d3.max(data, function(d) { return d.rating; })])
+  .range([0, width]);
 
 var chart = d3.select(".chart")
-    .attr("width", width)
-    .attr("height", height);
-
-var barWidth = width / data.length;
+  .attr("width", width)
+  .attr("height", barHeight * data.length);
 
 var bar = chart.selectAll("g")
-    .data(data)
+  .data(data)
   .enter().append("g")
-    .attr("transform", function(data, i) { return "translate(" + i * barWidth + "),0"; });
+  .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
 
 bar.append("rect")
-		.attr("y", function(d) { return y(d); })
-    .attr("height", function(d) { return height - y(d); })
-		.attr("width", barWidth - 1);
+  .attr("width", function(d) { return x(d.rating); })
+  .attr("height", barHeight - 1);
 
 bar.append("text")
-    .attr("x", barWidth / 2)
-    .attr("y", function(d) { return y(d) + 3; })
-    .attr("dy", ".75em")
-    .text(function(d) { return d; });
+  .attr("x", function(d) { return x(d.rating) - 3; })
+  .attr("y", barHeight / 2)
+  .attr("dy", ".35em")
+  .text(function(d) { return d.rating; });
+
+function type(d) {
+  d.rating = +d.rating; // coerce to number
+  return d;
+}
