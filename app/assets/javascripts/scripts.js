fdb790703cef827db2c1196dd38172bc1abc6448
Joe21 <joej21@gmail.com>
2014-03-20 18:29:42 -0400

    reads js object data in vertical

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -4,30 +4,35 @@ var data = [
   {"name": "World's Fair Marina", "rating": 3}
 ];
 
-var width = 420,
-  barHeight = 20;
+var width = 960,
+  height = 500;
 
-var x = d3.scale.linear()
-  .domain([0, d3.max(data, function(d) { return d.rating; })])
-  .range([0, width]);
+var x = d3.scale.ordinal()
+  .rangeRoundBands([0, width], .1)
+  .domain(data.map(function(d) { return d.name; }));
+
+var y = d3.scale.linear()
+  .range([height, 0])
+  .domain([0, d3.max(data, function (d) { return d.rating; })]);
 
 var chart = d3.select(".chart")
   .attr("width", width)
-  .attr("height", barHeight * data.length);
+  .attr("height", height);
 
 var bar = chart.selectAll("g")
   .data(data)
   .enter().append("g")
-  .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+  .attr("transform", function(d) { return "translate(" + x(d.name) + ",0)"; });
 
 bar.append("rect")
-  .attr("width", function(d) { return x(d.rating); })
-  .attr("height", barHeight - 1);
+  .attr("y", function(d) { return y(d.rating); })
+  .attr("height", function(d) { return height - y(d.rating); })
+  .attr("width", x.rangeBand());
 
 bar.append("text")
-  .attr("x", function(d) { return x(d.rating) - 3; })
-  .attr("y", barHeight / 2)
-  .attr("dy", ".35em")
+  .attr("x", x.rangeBand() / 2)
+  .attr("y", function(d) { return y(d.rating) + 3; })
+  .attr("dy", ".75em")
   .text(function(d) { return d.rating; });
 
 function type(d) {
