e133811022aa713d45493ad3a5ba094b00772e1f
Joe21 <joej21@gmail.com>
2014-03-21 20:10:59 -0400

    exploring different designs for json requests

--- app/controllers/tracker_controller.rb
+++ app/controllers/tracker_controller.rb
@@ -2,10 +2,26 @@ class TrackerController < ApplicationController
 	before_filter :authenticate_user!, only: [:index]
 	
 	def index
-		@trips_within_30_days = current_user.trips.where(date: (Time.now - 30.day)..Time.now)
 	end
 
-	def fire_for_effect
+	def target_front_end
+
+		# Obtain all trips within the past 30 days
+		all_trips_within_30 = Trip.where(date: (Time.now - 30.day)..Time.now)
+
+		stats = {location_name: "", rating: 0, frequency: 0 }
+		stat_holder = []
+		all_trips_within_30.each do |trip|
+			if stat_holder.include?(trip.location)
+				stat_holder
+
+		# location = ""
+		# rating = 0
+		# warhead = {}
+		# payload = []
+
+
+
 	end
 
 end
