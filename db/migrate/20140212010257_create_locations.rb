4c7b8ce0b672830c8ab78d8e9fda658b17e54fb1
Joe21 <joej21@gmail.com>
2014-03-27 20:25:35 -0400

    Prior to heroku init deployment

--- db/migrate/20140212010257_create_locations.rb
+++ db/migrate/20140212010257_create_locations.rb
@@ -1,7 +1,7 @@
 class CreateLocations < ActiveRecord::Migration
   def change
     create_table :locations do |t|
-    	t.string :name
+    	t.string :name, unique: true
     	t.boolean :verified, default: false
 
       t.timestamps
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,25 +2,23 @@ User.delete_all
 Location.delete_all
 Trip.delete_all
 
-# User.create(email:"joe@joe.com", password:"password", name:"Joe")
-# User.create(email:"john@john.com", password:"password", name:"John")
-# User.create(email:"lonebull@lonebull.com", password:"password", name:"Lonebull")
+User.create(email:"joe@joe.com", password:"password", name:"Joe", trips_counter: 0)
+User.create(email:"john@john.com", password:"password", name:"John", trips_counter: 0)
+User.create(email:"lonebull@lonebull.com", password:"password", name:"Lonebull", trips_counter: 0)
 Location.create(name:"World's Fair Marina", verified: true)
 Location.create(name:"Freeport", verified: true)
 Location.create(name:"Captree", verified: true)
 Location.create(name: "Montauk Marine Basin", verified: true)
 Location.create(name: "Point Lookout", verified: true)
 Location.create(name: "Sheepshead Bay", verified: true)
-# Trip.create(name: "test1", date: DateTime.new(2014,03,10), vessel:"Laura Lee", captain: "", rating:"4", comments:"Pretty good trip. Hit a lot of seabass")
-# Trip.create(name: "test2", date: DateTime.new(2014,03,01), vessel: "Restless", captain: "Fred", rating:"5", comments:"Killed it")
-# Trip.create(name: "test3", date: DateTime.new(2014,03,14), vessel:"NevrEnuff2", captain: "Nick", rating:"1", comments:"Really cold but slammed the bluefish...a couple stripers too")
-# Trip.create(name: "test4", date: DateTime.new(2014,03,15), vessel:"NevrEnuff2", captain: "Romeo", rating:"3", comments:"Weather sucked, the whole boat skunked, and one of the engines blew out so it took 3 hours to get back to port")
+Trip.create(name: "test1", date: DateTime.new(2014,03,10), vessel:"Laura Lee", captain: "", rating:"4", comments:"Pretty good trip. Hit a lot of seabass")
+Trip.create(name: "test2", date: DateTime.new(2014,03,01), vessel: "Restless", captain: "Fred", rating:"5", comments:"Killed it")
+Trip.create(name: "test3", date: DateTime.new(2014,03,14), vessel:"NevrEnuff2", captain: "Nick", rating:"1", comments:"Really cold but slammed the bluefish...a couple stripers too")
+Trip.create(name: "test4", date: DateTime.new(2014,03,15), vessel:"NevrEnuff2", captain: "Romeo", rating:"3", comments:"Weather sucked, the whole boat skunked, and one of the engines blew out so it took 3 hours to get back to port")
+
+User.all.each do |user| user.locations << Location.all end
 
 # Seed Testing
-# User.first.locations << Location.all
-# User.first.trips << Trip.all
-# User.all.each do |user| user.locations << Location.all end
-
 # joe = User.find_by_name("Joe")
 # john = User.find_by_name("John")
 # lonebull = User.find_by_name("Lonebull")
@@ -38,3 +36,16 @@ Location.create(name: "Sheepshead Bay", verified: true)
 # freeport.trips << test2
 # marina.trips << test3
 # marina.trips << test4
+
+# joe.trips << test1
+# joe.trips << test4
+# joe.update_attribute :trips_counter, 3
+# john.trips << test2
+# john.update_attribute :trips_counter, 2
+# lonebull.trips << test3
+# lonebull.update_attribute :trips_counter, 2
+
+
+### NOTES ###
+# git clone git@heroku.com:chartertracker.git -o heroku
+# http://chartertracker.herokuapp.com/
