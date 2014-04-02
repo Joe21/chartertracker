c0eb6a02f9924286c39cb771710577bbb4080571
Joe21 <joej21@gmail.com>
2014-04-01 22:01:04 -0400

    image size processing implemented for thumbs and regular

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -19,7 +19,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  # process :scale => [200, 300]
+  process :resize_to_fit => [680, 680]
   #
   # def scale(width, height)
   #   # do something
@@ -27,7 +27,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process :resize_to_fit => [200, 200]
+    process :resize_to_fill => [200, 200]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
